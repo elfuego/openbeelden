66ffc876dc92f3745d8fe0644a19ef91451fa8b6
André van Toly <andre@toly.nl>
2009-08-15 18:55:29 +0000

    OIP-37 Safari needs it be async: false

--- src/main/webapp/style/js/upload.js
+++ src/main/webapp/style/js/upload.js
@@ -1,13 +1,21 @@
 $(document).ready(function() {
     var progressUrl = $("head meta[name=ContextRoot]").attr("content") + "progress.jspx";
     $("form[enctype=multipart/form-data]").each(function() {
-
         $(this).submit(function() {
             var form = this;
+            var result;
             var progress = function() {
                 $(form).find(".progressInfo").each(function() {
-                    $(this).load(progressUrl);
+                    $.ajax({
+                        url: progressUrl,
+                        async: false,
+                        contentType: 'xml',
+                        complete: function(data) {
+                            result = data.responseText;
+                        }
+                    });
                 });
+                $(form).find(".progressInfo").html(result);
                 setTimeout(progress, 1000);
             };
             progress();
