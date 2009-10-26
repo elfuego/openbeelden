092cce05e82c3b204a850b09e9caf21a7219cea1
André van Toly <andre@toly.nl>
2009-10-26 22:31:38 +0000

    basic line-out in the comment about what it should do for rank project manager

--- src/main/java/eu/openimages/Authorization.java
+++ src/main/java/eu/openimages/Authorization.java
@@ -1,7 +1,11 @@
 package eu.openimages;
 
+import org.mmbase.bridge.Cloud;
+import org.mmbase.bridge.Node;
+
 import org.mmbase.security.implementation.cloudcontext.*;
 import org.mmbase.security.Operation;
+import org.mmbase.security.Rank;
 import org.mmbase.module.core.MMObjectNode;
 import org.mmbase.util.logging.Logger;
 import org.mmbase.util.logging.Logging;
@@ -38,6 +42,27 @@ public class Authorization extends Verify {
                     case Operation.DELETE_INT: return true;
                     }
                 }
+                
+                /* if contextNode's owner is een usernode met rank < project manager */
+                /*
+                Cloud cloud = org.mmbase.bridge.ContextProvider.getDefaultCloudContext().getCloud("mmbase", "class", null);
+                Node cuNode = cloud.getNode(contextNode.getNumber());
+                
+                if (cuNode.getNodeManager().getName().equals("mmbaseusers")) {
+                    Rank rank1 = UserProvider.getRank(userNode);
+                    Rank rank2 = UserProvider.getRank(cuNode);
+                    
+                    if (rank2.getInt() < rank1.getInt()) {
+                        log.debug("Project manager ?");
+                        switch(operation.getInt()) {
+                            case Operation.READ_INT:   return true;
+                            case Operation.WRITE_INT:  return true;
+                            case Operation.DELETE_INT: return true;
+                        }
+                    }
+                }
+                */
+               
                 return super.mayDoOnContext(userNode, contextNode, operation, checkOwnRights);
             }
 
