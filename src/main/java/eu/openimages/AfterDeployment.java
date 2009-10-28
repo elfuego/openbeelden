7807205f0befe8889325f494450f7ba5f529cc66
André van Toly <andre@toly.nl>
2009-10-28 15:26:22 +0000

    adding some more security configuration mainly for group project managers

--- src/main/java/eu/openimages/AfterDeployment.java
+++ src/main/java/eu/openimages/AfterDeployment.java
@@ -31,20 +31,23 @@ public class AfterDeployment implements Runnable {
         Cloud cloud = ContextProvider.getDefaultCloudContext().getCloud("mmbase", "class", null);
         CloudThreadLocal.bind(cloud);
         log.info("Running " + this + " with " + cloud);
+        
+        /* admin */
         Node adminNode    = SearchUtil.findNode(cloud, "mmbaseusers", "username", "admin");
+        Node adminGroup   = SearchUtil.findNode(cloud, "mmbasegroups", "name", "Administrators");
         Node adminContext = SearchUtil.findNode(cloud, "mmbasecontexts", "name", "admin");
-
         log.info("Changing default context and password of admin user");
         adminNode.setNodeValue("defaultcontext", adminContext);
         adminNode.setStringValue("password", "openimages2009");
         adminNode.commit();
 
-
+        /* site users */
         log.info("Revoking some rights of 'Users' on the 'default' context.");
         Node defaultContext   = SearchUtil.findNode(cloud, "mmbasecontexts", "name", "default");
         Node systemContext    = SearchUtil.findNode(cloud, "mmbasecontexts", "name", "system");
         Node siteusersContext = SearchUtil.findNode(cloud, "mmbasecontexts", "name", "siteusers");
         Node usersGroup       = SearchUtil.findNode(cloud, "mmbasegroups",   "name", "Users");
+        Node siteusersRank    = SearchUtil.findNode(cloud, "mmbaseranks", "name", "site user");
 
         ContextBuilderFunctions.revoke(defaultContext,  usersGroup, Operation.WRITE,  cloud.getUser());
         ContextBuilderFunctions.revoke(defaultContext,  usersGroup, Operation.DELETE, cloud.getUser());
@@ -56,11 +59,42 @@ public class AfterDeployment implements Runnable {
             cloud.getNodeManager(nm).setContext("siteusers");
         }
 
-
+        /* b+g */
         Node begNode    = SearchUtil.findNode(cloud, "mmbaseusers", "username", "beeldengeluid");
         Node begContext = SearchUtil.findNode(cloud, "mmbasecontexts", "name", "beeldengeluid");
+        begNode.setNodeValue("rank", siteusersRank);
+        begNode.commit();
         
+        /* project managers */
+        Node pmGroup = SearchUtil.findNode(cloud, "mmbasegroups", "name", "Project managers");
+        log.info("Granting some rights to group 'Project managers' #" + pmGroup.getNumber());
         
+        Node pmUser  = SearchUtil.findNode(cloud, "mmbaseusers", "username", "foofoo");
+        Node pmRank  = SearchUtil.findNode(cloud, "mmbaseranks", "name", "project manager");
+        pmUser.setNodeValue("rank", pmRank);
+        pmUser.commit();
+        
+        ContextBuilderFunctions.grant(defaultContext, pmGroup, Operation.CREATE, cloud.getUser());
+        ContextBuilderFunctions.grant(defaultContext, pmGroup, Operation.WRITE, cloud.getUser());
+        ContextBuilderFunctions.grant(defaultContext, pmGroup, Operation.DELETE, cloud.getUser());
+        ContextBuilderFunctions.grant(defaultContext, pmGroup, Operation.CHANGE_CONTEXT, cloud.getUser());
+        ContextBuilderFunctions.grant(defaultContext, pmGroup, Operation.CHANGE_RELATION, cloud.getUser());
+        
+        ContextBuilderFunctions.grant(begContext, pmGroup, Operation.CREATE, cloud.getUser());
+        ContextBuilderFunctions.grant(begContext, pmGroup, Operation.READ, cloud.getUser());
+        ContextBuilderFunctions.grant(begContext, pmGroup, Operation.WRITE, cloud.getUser());
+        ContextBuilderFunctions.grant(begContext, pmGroup, Operation.DELETE, cloud.getUser());
+        ContextBuilderFunctions.grant(begContext, pmGroup, Operation.CHANGE_CONTEXT, cloud.getUser());
+        ContextBuilderFunctions.grant(begContext, pmGroup, Operation.CHANGE_RELATION, cloud.getUser());
+        
+        ContextBuilderFunctions.grant(systemContext,  pmGroup, Operation.CREATE, cloud.getUser());
+
+        RelationManager rm = cloud.getRelationManager(cloud.getNodeManager("mmbasegroups"), cloud.getNodeManager("mmbasegroups"), "contains"); 
+        Node everybody     = SearchUtil.findNode(cloud, "mmbasegroups", "name", "Everybody");
+        everybody.createRelation(pmGroup, rm).commit();     // get rights from everybody
+        usersGroup.createRelation(pmGroup, rm).commit();    // get rights from users
+        pmGroup.createRelation(adminGroup, rm).commit();    // give rights to admins
+
         CloudThreadLocal.unbind();
 
     }
