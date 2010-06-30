1deb554bc01ab63561f55846c26fee30143f028f
André van Toly <andre@toly.nl>
2010-06-30 11:38:35 +0000

    trying to fix an issue in which Firefox does not show any progress but I have no idea, other browser seem fine

--- src/main/webapp/style/js/upload.js
+++ src/main/webapp/style/js/upload.js
@@ -5,34 +5,34 @@ $(document).ready(function() {
         $(this).submit(function() {
             var form = this;
             var result = "<div>Uploading...</div>";
+            $(pInfo).html(result);
             var i = 0;
             var progress = null;
-            window.clearInterval(progress);
-            progress = window.setInterval(function() {
+            clearInterval(progress);
+            progress = setInterval(function() {
                 $.ajax({
                     url: progressUrl,
                     async: false,
                     cache: false,
-                    contentType: 'xml',
                     error: function(xhr, status, err) {
                         result = '<div>Error: ' + status + " : " + err + '</div>';
                     },
                     complete: function(data) {
                         result = data.responseText;
+                        //console.log('complete');
                         if (result.indexOf('100%') > -1 && i == 0) {
-                            $(pInfo).html("<div>Uploading...</div>");
-                        } else {
-                            $(pInfo).html(result);
+                            result = "<div>Uploading...</div>";
                         }
                         i++;
                     },
                     success: function(data) {
                         result = data.responseText;
-                        $(pInfo).html(result);
-                        window.clearInterval(progress);
+                        //console.log('success');
                     }
                 });
-            }, 500);
+                $(pInfo).html(result);
+                //console.log('uploading: ' + i);
+            }, 1000);
         });
     });
 });
