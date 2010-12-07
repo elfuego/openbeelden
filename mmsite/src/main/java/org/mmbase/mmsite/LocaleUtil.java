0fb1315887d7f085514755784a37fb082a6ae1e6
André van Toly <andre@toly.nl>
2010-12-07 21:03:45 +0000

    removed debug

--- mmsite/src/main/java/org/mmbase/mmsite/LocaleUtil.java
+++ mmsite/src/main/java/org/mmbase/mmsite/LocaleUtil.java
@@ -77,7 +77,6 @@ public class LocaleUtil {
             }
             addDegraded(acceptedLocales);
         }
-        log.debug("set locales: " + acceptedLocales);
     }
     
     public List<String> getAcceptedLocaleStrings() {
