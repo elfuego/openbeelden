6ecb015be725cd38f655c480135cd99342e26868
André van Toly <andre@toly.nl>
2011-11-05 16:23:09 +0000

    trying to make this work:
      <mm:cloud authenticate="apikey" method="delegate">
        user: <mm:cloudinfo type="user" />
      </mm:cloud>
    
    little effect still.

--- src/main/java/eu/openimages/Authentication.java
+++ src/main/java/eu/openimages/Authentication.java
@@ -20,10 +20,13 @@ along with The Open Images Platform.  If not, see <http://www.gnu.org/licenses/>
 
 package eu.openimages;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.mmbase.module.core.MMObjectNode;
 import org.mmbase.security.implementation.cloudcontext.Authenticate;
 import org.mmbase.security.implementation.cloudcontext.BasicUserProvider;
 import org.mmbase.security.implementation.cloudcontext.UserProvider;
+import org.mmbase.security.implementation.cloudcontext.User;
 import org.mmbase.security.implementation.cloudcontext.UserStatus;
 import org.mmbase.util.functions.Parameter;
 import org.mmbase.util.functions.Parameters;
@@ -41,6 +44,7 @@ import org.mmbase.util.logging.Logging;
 public class Authentication extends Authenticate {
 
     private static final Logger log = Logging.getLoggerInstance(Authentication.class);
+    private boolean allowEncodedPassword = true;    /* apikey makes use of it */
 
     @Override
     public  UserProvider getUserProvider() {
@@ -73,9 +77,54 @@ public class Authentication extends Authenticate {
         return super.getDefaultMethod(protocol);
         //return METHOD_SESSIONDELEGATE; // redirect to /login
     }
+    
     @Override
     public org.mmbase.security.UserContext login(String application, java.util.Map<String, ?> loginInfo, Object[] parameters) throws SecurityException {
+        UserProvider users = getUserProvider();
+        if (users == null) {    
+            throw new SecurityException("builders for security not installed, if you are trying to install the application belonging to this security, please restart the application after all data has been imported");
+        }
 
+        allowEncodedPassword = org.mmbase.util.Casting.toBoolean(users.getUserBuilder().getInitParameter("allowencodedpassword"));
+        if (application.equals("apikey")) {
+            log.debug("application " + application);
+            log.debug("users " + users + ", allowEncodedPassword " + allowEncodedPassword);
+            
+            String apikey = (String) loginInfo.get("apikey");
+            
+            if (apikey == null || "".equals(apikey)) {
+                HttpServletRequest req = (HttpServletRequest) loginInfo.get(Parameter.REQUEST.getName());
+                apikey = (String) req.getAttribute("apikey");
+            }
+            
+            log.debug("API key is " + apikey);
+            
+            /* call method for API key: get username and password from API key */
+            String username = "foofoo";
+            String password = "5426824942db4253";
+            
+            // login with provided credentials with "name/encodedpassword"
+            // below copied from name/encodedpassword in super 
+            if (username == null || password == null) {
+                log.debug("SecurityException 1");
+                throw new SecurityException("Expected the property 'username' and 'password'. But received " + loginInfo);
+            }
+            MMObjectNode node = users.getUser(username, password, true);
+            if (node != null && ! users.isValid(node)) {
+                log.debug("SecurityException 2");
+                throw new SecurityException("Logged in an invalid user");
+            }
+            log.debug("user " + node);
+            if (node == null) return null;
+            return new User(node, getKey(), application);
+            
+            // TODO: put creditials from apikey in logInfo map
+            //loginInfo.put("username", username);
+            //loginInfo.put("encodedpassword", password);
+            
+            // switch application to name/encodedpassword
+            //application = "name/encodedpassword";
+        }
         return super.login(application, loginInfo, parameters);
     }
 
@@ -83,8 +132,8 @@ public class Authentication extends Authenticate {
     @Override
     public Parameters createParameters(String application) {
         application = application.toLowerCase();
-        if ("APIToken".equals(application)) {
-            return new Parameters(new Parameter("apitoken", String.class));
+        if ("apikey".equals(application)) {
+            return new Parameters(Parameter.REQUEST, new Parameter("apikey", String.class));
         } else {
             return super.createParameters(application);
         }
@@ -92,7 +141,19 @@ public class Authentication extends Authenticate {
 
     @Override
     public String[] getTypes(int method) {
-        // Add APIToken
-        return super.getTypes();
+        if (allowEncodedPassword) {
+            if (method == METHOD_ASIS) {
+                return new String[] {"anonymous", "name/password", "name/encodedpassword", "class", "apikey"};
+            } else {
+                return new String[] {"name/password", "name/encodedpassword", "class", "apikey"};
+            }
+        } else {
+            if (method == METHOD_ASIS) {
+                return new String[] {"anonymous", "name/password", "class", "apikey"};
+            } else {
+                return new String[] {"name/password", "class", "apikey"};
+            }
+        }
+
     }
 }
