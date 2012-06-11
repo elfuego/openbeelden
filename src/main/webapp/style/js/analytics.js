ab001bf3143ac8fd2e4eb0a8baeb1373e7f77f0e
André van Toly <andre@toly.nl>
2012-06-11 11:02:24 +0000

    analytics label is the url of downloaded file, action contains nodenumber nodelabel and mimetype f.e. 1234 hd video/mp4

--- src/main/webapp/style/js/analytics.js
+++ src/main/webapp/style/js/analytics.js
@@ -3,18 +3,23 @@ Track downloads with Google Analytics
 https://developers.google.com/analytics/devguides/collection/gajs/methods/gaJSApiEventTracking
 
 category    Downloads
-action      title tag of download link, f.e. 'hd 756x554 video/mp4'
+action      nodenumber media item plus title tag of download link, f.e. '786 hd video/mp4'
 opt_label   (a.k.a. Event Label) url of downloaded file
 opt_value   Integer, event value (empty)
 */
 $(document).ready(function(){
     if ($('#download').length) {
         $('#download').find('a').click(function(ev){
-            //ev.preventDefault();
             var url = ev.target.href;
+            var page = window.location.pathname;
+            var result = page.match(/\/(\d+)\//);
+            if (result != null) { 
+                var media = result[1]; 
+            } else { var media = page; }
             var title = $(this).attr('title');
-            //console.log('t: ' + title + ' url: ' + url);
-            _gaq.push(['_trackEvent', 'Downloads', title, url]);
+            var label = media + " " +  title;
+            //console.log('url: ' + url + ' label: ' + label);
+            _gaq.push(['_trackEvent', 'Downloads', url, label]);
         });
     }
 });
