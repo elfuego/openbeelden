1f915e19feebaa332bb9ccdfa9b6b9dac31915af
André van Toly <andre@toly.nl>
2012-11-01 16:22:32 +0000

    updated version of upload.js to that of the one in streams app

--- src/main/webapp/style/js/upload.js
+++ src/main/webapp/style/js/upload.js
@@ -1,20 +1,44 @@
-$(document).ready(
-    function() {
+$(document).ready(function() {
 	var lang = $("html").attr("xml:lang");
 	var progressUrl = $("head meta[name=ContextRoot]").attr("content") + "action/progress.jspx?lang=" + lang;
 	$("form.mm_form").each(function() {
 		var pInfo = $(this).find(".progressInfo").first();
 		$(this).submit(function() {
 			var form = this;
+			var result = "<div>Uploading...</div>";
+			$(pInfo).html(result);
 			var i = 0;
 			var progress = null;
 			clearInterval(progress);
-			var progress = setInterval(function(){ $(pInfo).load(progressUrl); }, 1000);
-	    });
-	});
-	/* Just some stuff to guess a nicer title */
-	$("input[type=file]").change(
-	    function(ev) {
+			progress = setInterval(function() {
+				$.ajax(
+				    { url: progressUrl,
+				      async: false,
+				      cache: false,
+				      error: function(xhr, status, err) {
+					  result = '<div>Error: ' + status + " : " + err + '</div>';
+				      },
+				      complete: function(data) {
+					  result = data.responseText;
+					  //console.log('complete');
+					  if (result.indexOf('100%') > -1 && i == 0) {
+					      result = "<div>Uploading...</div>";
+					  }
+					  i++;
+				      },
+				      success: function(data) {
+					  result = data.responseText;
+					  //console.log('success');
+				      }
+				    });
+				$(pInfo).html(result);
+				//console.log('uploading: ' + i);
+            }, 1000);
+            return true;
+        });
+    });
+    /* Just some stuff to guess a nicer title */
+	$("input[type=file]").change(function(ev) {
 		var file = ev.target.value;
 		var title = $(ev.target.form).find(".mm_validate.mm_f_title")[0];
 		if (title.originalValue == $(title).val()) {
